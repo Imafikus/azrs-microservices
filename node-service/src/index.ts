d13b9e34bac4e15faf3c7fb9b2ea36b5e87a589a
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-12-21 16:48:41 +0100

    Initialize project using create-express-typescript-application

--- node-service/src/index.ts
+++ node-service/src/index.ts
@@ -1,16 +1,9 @@
-import express, { Request, Response } from 'express'
+import app from './server';
+import config from '../config.json';
 
-const app = express()
-
-const port = 5006
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/ping', (req: Request, res: Response) => {
-    res.send('ping')
-})
-
+// Start the application by listening to specific port
+const port = Number(process.env.PORT || config.PORT || 8080);
 app.listen(port, () => {
-    console.log(`App is listening on port ${port} !`)
-})
+  console.info('Express application started on port: ' + port);
+});
+
